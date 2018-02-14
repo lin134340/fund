e3dd1549fd70cb045242d05a19221634816b585e
林超031211 <031211@CITICSO.COM>
2018-02-14 09:23:42 +0800

    t

--- t.py
+++ t.py
@@ -2,15 +2,28 @@ import requests
 import json
 import pprint
 
-def pull_fund():
-    url="https://www.jisilu.cn/jisiludata/etf.php?rp=25&page=1"
+def update_fund_list():
+    url = "https://www.jisilu.cn/jisiludata/etf.php?rp=25&page=1"
     response = requests.get(url)
     etf_index = json.loads(response.content)
     pprint.pprint(etf_index)
-    url="https://www.jisilu.cn/data/lof/index_lof_list/?rp=25&page=1"
+    url = "https://www.jisilu.cn/data/lof/index_lof_list/?rp=25&page=1"
     response = requests.get(url)
     lof_index = json.loads(response.content)
     pprint.pprint(lof_index)
     
+def update_fund(fund_id=None):
+    url="https://www.jisilu.cn/data/etf/detail_hists/"
+    data = {
+        "is_search":1,
+        "fund_id":fund_id,
+        "rp":50,
+        "page":1,
+    }
+    response = requests.post(url,data=data)
+    fund = json.loads(response.content)
+    pprint.pprint(fund)
+
 if __name__ == '__main__':
-    pull_fund()
+    # update_fund_list()
+    update_fund(fund_id=159901)
