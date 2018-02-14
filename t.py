de8836e6d617eb74aaacd5fab09205b99c6214b9
林超031211 <031211@CITICSO.COM>
2018-02-14 17:36:42 +0800

    a

--- t.py
+++ t.py
@@ -2,15 +2,29 @@ import requests
 import json
 import pprint
 
+g = {
+    "etf_index_fund_fn":"etf_index_fund",
+}
+
 def update_fund_list():
     url = "https://www.jisilu.cn/jisiludata/etf.php?rp=25&page=1"
     response = requests.get(url)
-    etf_index = json.loads(response.content)
-    pprint.pprint(etf_index)
-    url = "https://www.jisilu.cn/data/lof/index_lof_list/?rp=25&page=1"
-    response = requests.get(url)
-    lof_index = json.loads(response.content)
-    pprint.pprint(lof_index)
+    etf_index_fund_ori = json.loads(response.content)
+    try:
+        etf_index_fund_native = json.load(open(g["etf_index_fund_fn"],"r"))
+    except:
+        etf_index_fund_native = []
+    for r in etf_index_fund_ori["rows"]:
+        if all(map(lambda p: p["id"]!=r["id"], etf_index_fund_native)):
+            f = {
+                "id": r["id"],
+                "name": r["cell"]["fund_nm"],
+                "index_id": r["cell"]["index_id"],
+            }
+            etf_index_fund_native.append(f)
+            print("add fund:")
+            pprint.pprint(f)
+    json.dump(etf_index_fund_native, open(g["etf_index_fund_fn"],"w"))
     
 def update_fund(fund_id=None):
     url="https://www.jisilu.cn/data/etf/detail_hists/"
@@ -21,9 +35,22 @@ def update_fund(fund_id=None):
         "page":1,
     }
     response = requests.post(url,data=data)
-    fund = json.loads(response.content)
-    pprint.pprint(fund)
+    fund_ori = json.loads(response.content)
+    try:
+        fund_native = json.load(open(fund_id,"r"))
+    except:
+        fund_native = []
+    for r in fund_ori["rows"]:
+        if all(map(lambda p: p["hist_dt"]!=r["cell"]["hist_dt"], fund_native)):
+            fund_native.append(r["cell"])
+            print(fund_id, "add fund date:")
+            pprint.pprint(r["cell"])
+    json.dump(fund_native, open(fund_id,"w"))
+
+def main():
+    update_fund_list()
+    update_fund(fund_id="159901")
+
 
 if __name__ == '__main__':
-    # update_fund_list()
-    update_fund(fund_id=159901)
+    main()
